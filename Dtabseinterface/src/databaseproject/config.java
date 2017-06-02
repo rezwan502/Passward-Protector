eae76580c411de5ee2177eba2e800f14a94a3741
Md. Rezwanul Islam <rezwan.sec@gmail.com>
2017-06-02 16:51:29 +0600

    Add files via upload

---
+++ Dtabseinterface/src/databaseproject/config.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package databaseproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Ahmed
+ */
+class config {
+    static Connection con;
+    public  static Connection db_connect(){
+        try {
+             con=DriverManager.getConnection("jdbc:mysql://localhost:3306/account&password","root","");
+             System.out.println("Connected");
+             return con;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+    
+}
